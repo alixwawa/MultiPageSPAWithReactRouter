bff6f728754456b398edb4bd5ab8430d12eca36f
Alix Wawa <alixwawa@yahoo.com>
2023-05-23 12:17:00 -0500

    showing error pages with errorElement

---
+++ src/pages/Error.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ErrorPage = () => {
+	return (
+		<>
+			<main>
+				<h1>An error occured!</h1>
+				<p>Could not find this page!</p>
+			</main>
+		</>
+	);
+};
+
+export default ErrorPage;
